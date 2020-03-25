e4478571827cf64804106472700fabc39d1fd568
sungwon268 <sungwon268@gmail.com>
2020-03-26 01:11:49 +0900

    subarray_sum.py

--- subarray_sum.py
+++ subarray_sum.py
@@ -1,21 +1,17 @@
 arr = [-2, 1, -3, 4, -1, 2, 1,-5, 4]
 
-max_sum = arr[0]
-total_max = arr[0]
-start, end, t_start, t_end = 0, 0, 0, 0
+max_sum = arr[0]  # 1
+total_max = arr[0]  # 1
 
-for i in range(1, len(arr)):
-    arr_end = arr[i]
-    max_sum += arr[i]
+for i in range(1, len(arr)):   # n-1
+    end = i   # n-1
+    max_sum += arr[i]  # n-1
 
-    if (max_sum < arr_end):
-        max_sum = arr_end
-        start = i
-        end = i
-    else:
-        end = i
+    if (max_sum < arr[end]):  #n-1
+        max_sum = arr[end]  #1
+        start = i             #1
 
-    if (total_max < max_sum):  #
+    if (total_max < max_sum):  # n-1
         total_max = max_sum
         t_start = start
         t_end = end
