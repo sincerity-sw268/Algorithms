c935665d06f7baea4fac0aefa7e0930289d639c7
sungwon268 <sungwon268@gmail.com>
2020-03-28 22:00:59 +0900

    Insertion Sort

---
+++ InsertionSort.py
@@ -0,0 +1,13 @@
+num = list(map(int,input().split()))
+
+i=0
+j=0
+for i in range(1,len(num)):
+    key = num[i]
+    j = i - 1
+    while ( j >= 0 and num[j]>key):
+        num[j+1]=num[j]
+        j = j-1
+    num[j+1] = key
+
+print(num)
